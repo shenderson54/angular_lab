5483756b6593625b9f6898cc7d9c76ddef3d1990
SHenderson <shenderson5454@gmail.com>
2021-04-01 15:40:48 -0400

    Getting ready for turn

---
+++ src/app/todo.ts
@@ -0,0 +1,7 @@
+import { TodoComponent } from "./todo/todo.component";
+
+export interface Todo {
+    task: string;
+    completed: boolean;
+}
+
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from './app-todo-info';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+    todos: Todo[] = [
+    { task: 'Do laundry', completed: false },
+    { task: 'Take a walk', completed: true },
+    { task: 'Make dinner', completed: false },
+    { task: 'Finish homework', completed: true },
+    { task: 'Play Call of Duty', completed: false },
+    { task: 'Catch up on my TV shows', completed: false },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  complete(todo: Todo) {
+    todo.completed = true
+  }
+}
